3007d899806f5f65d98831db5917a2ef19136d8b
ildenio <ildeniocosta@gmail.com>
2019-07-02 00:46:59 -0300

    Categoria repository.

---
+++ src/main/java/com/ildenio/curso/repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package com.ildenio.curso.repositories;
+
+import com.ildenio.curso.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
